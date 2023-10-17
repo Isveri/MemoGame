0e3504cdb2e6532bfa11950c6c4a138aa16a829e
Patryk Iwanowski <patryk.iwanowski00@gmail.com>
2023-10-17 12:43:56 +0200

    lab3 card changes

--- MemoGame/CardView.swift
+++ MemoGame/CardView.swift
@@ -5,4 +5,23 @@
 //  Created by student on 17/10/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct CardView: View {
+    let content: String
+            @State var isFaceUp : Bool = true
+            var body: some View {
+                ZStack{
+                    let base = RoundedRectangle(cornerRadius: 24)
+                    Group {
+                        base.fill(Color.white)
+                        base.strokeBorder(lineWidth: 2)
+                        Text(content).font(.largeTitle)
+                    
+                }.opacity(isFaceUp ? 1 : 0)
+                base.fill().opacity(isFaceUp ? 0 : 1)
+                }
+                .foregroundColor(.blue)
+                .onTapGesture(perform: {isFaceUp.toggle()})
+            }
+}
--- MemoGame/ContentView.swift
+++ MemoGame/ContentView.swift
@@ -8,15 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojis = ["😃","🐸","🐯","🪲","🪱","🐹","😃","🐸","🐯","🪲","🪱","🐹"]
+    @State var numberOfCards = 2
+
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            ScrollView{
+                cardDisplay
+            }
+           cardAdjuster
+        }.padding()
     }
+    
+    
+    var cardAdjuster: some View {
+        HStack {
+            cardAdder
+            Spacer()
+            cardRemover
+        }
+    }
+    
+    func adjustCardNumber(by offset: Int, symbol: String) -> some View{
+        return Button(symbol){
+            numberOfCards += offset
+        }.frame(width: 50)
+        .border(Color.blue)
+        .disabled(numberOfCards <= 2 && symbol == "-" || numberOfCards >= emojis.count && symbol == "+")
+    }
+    
+    var cardDisplay: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+                    ForEach(0..<numberOfCards, id: \.self){
+                        index in
+                        CardView(content: emojis[index])
+                        .aspectRatio(11/3, contentMode: .fit)
+                    }
+                }.foregroundColor(Color.blue)
+    }
+    var cardAdder: some View {
+        adjustCardNumber(by: 2, symbol: "+")
+    }
+    
+    var cardRemover: some View {
+        adjustCardNumber(by: -2, symbol: "-")
+    }
+    
+
+    
 }
 
 #Preview {
